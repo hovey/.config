d41c85646ef345413e5932535074ae1639c05280
hovey <hovey@me.com>
2021-07-19 10:50:44 -0600

    update sparta to Python 3.9

--- fish/config_sparta.fish
+++ fish/config_sparta.fish
@@ -1,6 +1,9 @@
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source  # <-- now antiquated as old Python 3.8
 # <<< conda initialize <<<
+
+# 2021-07-18 Update Miniconda to Python 3.9
+eval /Users/sparta/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 
