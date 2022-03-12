7889a0cf6cd0ccfe31faf21d7b1e73328499587b
hovey <hovey@me.com>
2022-03-12 12:41:54 -0700

    comment out sparta specific

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,9 @@
 # 2021-07-18 Update Miniconda to Python 3.9
 # eval /Users/sparta/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 
-# 2021-07-19 Update s1060600 to Python 3.9
-eval /Users/chovey/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/sparta/.ghcup/bin $PATH # ghcup-env
+
+# 2022-02-17 
+# https://github.com/oh-my-fish/theme-bobthefish/blob/master/functions/fish_prompt.fish
+set -g theme_nerd_fonts yes
 
