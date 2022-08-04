dc2892dfbd2bb85f0b1e52df2603577148184f52
Chad Hovey <chovey@sandia.gov>
2022-08-04 17:10:48 -0600

    suppress right prompt for now

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,7 +1,5 @@
 function fish_right_prompt
   set_color blue
-  # date '+%Y%M%D_%H_%M'
-  # echo -n (date +%Y.%M.%D.%H:%M:%S)
-  echo (date +%Y-%m-%d) (date +%H:%M:%S)
-  set_color normal
+  # echo (date +%Y-%m-%d) (date +%H:%M:%S)
+  # set_color normal
 end
