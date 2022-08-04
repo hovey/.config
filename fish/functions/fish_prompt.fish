82fdb8857c8c724cc10dfe1828ed727ec5404c5d
Chad Hovey <hovey@me.com>
2022-08-04 17:06:00 -0600

    red greater than prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,5 +3,5 @@ function fish_prompt --description 'hoveyfish'
   # set_color blue
   # date 
   set_color yellow
-  printf '%s%s%s@%s%s %s%s%s > ' (set_color cyan) $USER (set_color yellow) (set_color purple) (prompt_hostname) (set_color yellow) (pwd) (set_color normal)
+  printf '%s%s%s@%s%s%s%s%s> ' (set_color yellow) $USER (set_color cyan) (set_color purple) (prompt_hostname) (set_color yellow) (pwd) (set_color red)
 end
