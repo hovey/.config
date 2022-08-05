43c2b8ea5dbce057860bd9b355091a70ed9ab046
Chad Hovey <hovey@me.com>
2022-08-05 12:03:10 -0600

    change host to dark purple color

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,6 +2,8 @@ function fish_prompt --description 'hoveyfish'
   # echo (pwd) '>' (set_color normal)
   # set_color blue
   # date 
+  # color 62A is eggplant, kind of a dark purple, which is nice
+  # https://fishshell.com/docs/current/cmds/set_color.html
   set_color yellow
-  printf '%s%s%s@%s%s%s%s%s> ' (set_color yellow) $USER (set_color cyan) (set_color purple) (prompt_hostname) (set_color yellow) (pwd) (set_color red)
+  printf '%s%s%s@%s%s%s%s%s> ' (set_color yellow) $USER (set_color blue) (set_color 62A) (prompt_hostname) (set_color yellow) (pwd) (set_color red)
 end
