93e4df12e74d2f67be25a548e37ea561b75d9019
Chad Brian Hovey <chovey@sandia.gov>
2023-10-18 10:38:33 -0600

    bash and zsh shell settings additional

--- bash/cbh-bashrc.sh
+++ bash/cbh-bashrc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# 2023-10-18 begin - build SSM
-export http_proxy=http://wwwproxy.sandia.gov:80
-export ftp_proxy=http://wwwproxy.sandia.gov:80
-export https_proxy=http://wwwproxy.sandia.gov:80
-export no_proxy=localhost,localnets,.sandia.gov,127.0.0.1,169.254.0.0/16,forge.sandia.gov
-# 2023-10-18 end
+echo "This is ~/.config/bash/cbh-bashrc.sh"
+
+# # 2023-10-18 begin - build SSM
+# export http_proxy=http://wwwproxy.sandia.gov:80
+# export ftp_proxy=http://wwwproxy.sandia.gov:80
+# export https_proxy=http://wwwproxy.sandia.gov:80
+# export no_proxy=localhost,localnets,.sandia.gov,127.0.0.1,169.254.0.0/16,forge.sandia.gov
+# # 2023-10-18 end
