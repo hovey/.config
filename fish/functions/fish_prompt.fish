ca24bae47b35cf9f9b5430ad3846e6f92b77525d
Chad Hovey <hovey@me.com>
2023-11-15 14:56:24 -0700

    add git branch to prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,5 +5,12 @@ function fish_prompt --description 'hoveyfish'
   # color 62A is eggplant, kind of a dark purple, which is nice
   # https://fishshell.com/docs/current/cmds/set_color.html
   set_color yellow
-  printf '%s%s%s@%s%s%s%s%s> ' (set_color yellow) $USER (set_color blue) (set_color 62A) (prompt_hostname) (set_color yellow) (pwd) (set_color red)
+  printf '%s%s %s%s%s@%s%s%s%s%s> ' \
+	  (set_color green) \
+	  (fish_git_prompt) \
+	  (set_color yellow) $USER \
+	  (set_color cyan) \
+	  (set_color 62A) (prompt_hostname) \
+	  (set_color yellow) (pwd) \
+	  (set_color red)
 end
